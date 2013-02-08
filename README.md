15e893bb59995bf5bf78a3adb1c223529acb6773
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2013-02-08 18:54:14 +0100

    Added custom builds of Trello.NET and Mite.NET.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 TrelloMite
 ==========
+
+Simple .NET library to connect Trello (https://trello.com/) to Mite (https://mite.yo.lk/) by parsing Trello cards & comments for specific time-tracking commands.
+
+Makes heavy use of  https://github.com/dillenmeister/Trello.NET and https://github.com/ccellar/mite.net. Since both libraries had bugs and/or missing features I'm using custom builds at the moment, instead of the NuGet packages. This will change as soon as my pull requests are accepted or the features/bugfixes have been added.
+
+Usage
+-----
+tba
+
+Trello Syntax
+-------------
+tba
---
+++ TrelloMite/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TrelloMite")]
+[assembly: AssemblyDescription("Library to connect Trello to Mite.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrelloMite")]
+[assembly: AssemblyCopyright("Copyright © Hannes Sachsenhofer")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("777bca9c-22b7-4a2e-b3d4-20ade635c4ba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
